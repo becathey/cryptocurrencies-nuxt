66ae46b65f8e82e55234702721a9dbb585e72848
Bill Cathey <billcathey@hotmail.com>
2022-09-01 22:55:59 -0400

    Add styles to header, nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,24 @@
 <template>
     <nav>
-        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/">CryptoMonitor</NuxtLink>
     </nav>
     <main>
         <slot />
     </main>
 </template>
+
+<style scoped>
+nav {
+    background-color: hsl(210, 90%, 30%);
+    padding: 1rem;
+    font-variant: small-caps;
+    font-size: 1.25rem;
+}
+nav a {
+    text-decoration: none;
+    color: white;
+}
+nav a:hover {
+    border-bottom: 1px solid hsl(210, 90%, 95%);
+}
+</style>
